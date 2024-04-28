375665815818e8d4e056cc001a1f02ce490e3300
Ahmed alkhatim Awadeljeed <alkhatim40@gmail.com>
2024-04-28 08:42:33 +0200

    set delete alert

---
+++ src/app/users/DeleteAlert.js
@@ -0,0 +1,25 @@
+'use client'
+import UsersApi from "@/api/users"
+import { useState } from "react"
+
+export default  () => {
+    const [userDeleted, setUser] = useState()
+    const [isVisible, setIsVisible] = useState(false)
+    UsersApi.on('userDeleted', (e) => { 
+        setUser(e.detail)
+        setIsVisible(true) 
+    })
+    
+    return(
+        <div>
+            { isVisible && <div class="modal fixed left-0 bottom-0 right-0 top-0  overflow-scroll   z-50 bg-[rgba(51,48,60,.1)]" >
+                <div class="modal-card  z-50 ">
+                    <div className="bg-[#fff] rounded-[8px] px-20 py-5">
+                        <p>{'The user ('+ userDeleted.username + ') Deleted Successfully'}</p>
+                        <div onClick={() => setIsVisible(false)} className="absolute top-[10px] right-[10px] cursor-pointer">x</div>
+                    </div>
+                </div>
+            </div>}
+        </div>
+    )
+}
--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,12 +1,14 @@
 import Users from "./Users"
 import SearchUsers from "./SearchUsers"
 import UsersProvider from "../../data/users"
+import DeleteAlert from "./DeleteAlert"
 
 export default () => (
     <div className="mt-10 mx-10 space-y-10">
         <UsersProvider>
             <SearchUsers />
             <Users />
+            <DeleteAlert />
         </UsersProvider>
     </div>
 )
